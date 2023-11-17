be14c6d07655b751cc9185cb9e8fe7e14eb77640
Gabrizinha <gabiemanu18@gmail.com>
2023-11-16 23:42:23 -0300

    commit

---
+++ src/main/java/br/edu/fateczl/SistemaGerenciamentoEstoque/model/Produto.java
@@ -0,0 +1,100 @@
+package br.edu.fateczl.SistemaGerenciamentoEstoque.model;
+
+import java.sql.Date;
+
+public class Produto {
+
+	private int codigo;
+	private String nome;
+	private int quantidade;
+	private Date vencimento;
+	private String marca;
+	private String categoria;
+	private String cnpjFornecedor;
+	private String nomeFornecedor;
+	private Date dataCompra;
+	private float valorUnitario;
+	private String descricao;
+	private float valorTotal;
+	private String nivel;
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Date getVencimento() {
+		return vencimento;
+	}
+	public void setVencimento(Date vencimento) {
+		this.vencimento = vencimento;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getCnpjFornecedor() {
+		return cnpjFornecedor;
+	}
+	public void setCnpjFornecedor(String cnpjFornecedor) {
+		this.cnpjFornecedor = cnpjFornecedor;
+	}
+	public String getNomeFornecedor() {
+		return nomeFornecedor;
+	}
+	public void setNomeFornecedor(String nomeFornecedor) {
+		this.nomeFornecedor = nomeFornecedor;
+	}
+	public Date getDataCompra() {
+		return dataCompra;
+	}
+	public void setDataCompra(Date dataCompra) {
+		this.dataCompra = dataCompra;
+	}
+	public float getValorUnitario() {
+		return valorUnitario;
+	}
+	public void setValorUnitario(float valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public float getValorTotal() {
+		return valorTotal;
+	}
+	public void setValorTotal(float valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+	public String getNivel() {
+		return nivel;
+	}
+	public void setNivel(String nivel) {
+		this.nivel = nivel;
+	}
+	
+	
+}
